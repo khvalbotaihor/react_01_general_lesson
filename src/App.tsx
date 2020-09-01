812b715462f87c0a448622961606ff1ac004725a
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-02 00:46:56 +0300

    4 lesson - add additional expand logic for Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,8 @@ function App() {
             <PageTitle title={"My friends"}/>
             Article 1
             <Rating value={3}/>
-            <Accordion titleValue={"Menu"}/>
-            <Accordion titleValue={"Users"}/>
+            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={false}/>
             Article 2
             <Rating value={0}/>
             <Rating value={1}/>
