ee997d319dd0f5c5fa480e36261b3e398ff59f55
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-01 16:58:24 +0300

    3 lesson - transferred Accordion componene t to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Acccordion/Accordion";
 
 
 function hello() {
@@ -76,36 +77,7 @@ function AppTitle() {
     )
 }
 
-function Accordion() {
-    console.log("Accordion rendering");
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
 
-function AccordionTitle() {
-    console.log("AccordionTitle rendering");
-
-    return (<h3>Menu</h3>
-    )
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering");
-
-    return (
-        <>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </>
-    )
-}
 
 
 export default App;
