0f742ba1d850338a991eb344c6590abf3eeb66d1
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-03 16:24:34 +0300

    8 lesson - add callback to Star function in Uncontrolled component

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -7,26 +7,25 @@ export function UncontrolledRating() {
 
     return (
         <div>
-            <Star selected={value>0}/><button onClick={()=>{setValue(1)}}>1</button>
-            <Star selected={value>1}/><button onClick={()=>{setValue(2)}}>2</button>
-            <Star selected={value>2}/><button onClick={()=>{setValue(3)}}>3</button>
-            <Star selected={value>3}/><button onClick={()=>{setValue(4)}}>4</button>
-            <Star selected={value>4}/><button onClick={()=>{setValue(5)}}>5</button>
+            <Star selected={value>0} setValue={setValue} value={1}/>
+            <Star selected={value>1} setValue={setValue} value={2}/>
+            <Star selected={value>2} setValue={setValue} value={3}/>
+            <Star selected={value>3} setValue={setValue} value={4}/>
+            <Star selected={value>4} setValue={setValue} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    value: 1|2|3|4|5
+    setValue : (value:1|2|3|4|5)=> void
 }
 
 function Star(props:StarPropsType) {
     console.log("Start rendering")
 
-/*    if(props.selected===true){
-        return <span><b>start</b> </span>
-    }else {
-        return <span>start</span>
-    }*/
-    return( props.selected ?  <span><b>start</b> </span> :  <span>start</span> )
+    return <span onClick={()=>{props.setValue(props.value)} }>
+        { props.selected ?  <b>start </b> : "start " }
+    </span>
 }
